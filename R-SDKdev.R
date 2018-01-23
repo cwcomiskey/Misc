fcfad5e6b9a935541e939977a46930cd6dc23a04
Chris Comiskey <cwcomiskey@gmail.com>
2018-01-23 15:26:23 -0800

    First FS connection with my SDK

--- R-SDKdev.R
+++ R-SDKdev.R
@@ -7,8 +7,7 @@
 
 install.packages("roxygen2"); library(roxygen2); install.packages("devtools"); library(devtools)
 
-devtools::install(".") # load home directory package
-devtools::install("./swagger") # 
+devtools::install("./R-SDK2.0/swagger") # 
 
 library(help = "swagger") 
 
@@ -20,7 +19,7 @@ library(help = "swagger")
 # Stable Version, swagger-codegen-2.2.3:
 # CLI: swagger-codegen generate -i fastscore-api-specs/suite-proxy-v1.yaml -l python
 
-# API Client  ========
+# ignore self-certify  ========
 # Had to use, from ApiClient class:
 # httr::GET(url = paste0(basePath, urlPath),
 #           config = httr::config(ssl_verifypeer = FALSE))
@@ -29,29 +28,37 @@ library(help = "swagger")
 
 httr::set_config(httr::config(ssl_verifypeer = FALSE)) # global ignore-self-certify config
 
+
 # ApiClient$new(...) =======
-# R client for FS API -- swg = instance of R6 class 'ApiClient'
-    # NOTICE: ** $new **
-swg <- swagger::ApiClient$new(basePath = "https://localhost:15080/api/1/service")
-    
+# R client for FS API -- api_cli = instance of R6 class 'ApiClient'
+api_cli <- swagger::ApiClient$new(
+  basePath = "https://localhost:15080/api/1/service") 
+
+
 # ConnectApi$new(...) ======
 # R6 'ConnectApi' class instance, (parent class = 'ApiClient', instance = 'swg')
-  con <- swagger::ConnectApi$new(apiClient = swg) # NOTICE: ** $new **
-        health <- con$health_get(instance = "connect") # instance == parameter (service)
-        con_get <- con$connect_get(instance = "connect")
+swg <- swagger::ConnectApi$new(apiClient = api_cli) 
+  health <- swg$health_get(instance = "connect") # instance == FS_service
+  con_get <- swg$connect_get(instance = "connect")
 
 # ModelManageApi$new(...) =======
-# Instance of R6 'ModelManageApi' class, with parent class/instance = 'ApiClient'/'swg'
-  # NOTICE: ** $new **
-  modman <- swagger::ModelManageApi$new(apiClient = swg) # instantiate class req's parent class arg
-        modlist <- modman$model_list(instance = "model-manage-1") 
-              modlist$content # [1] "hello-world"
-              modman$model_get(instance = "model-manage-1", model = "hello-world") # ******
-              stream_test <- modman$stream_get(instance = "model-manage-1", stream = "rest-in")
-               
-      
-        strmlist <- modman$stream_list(instance = "model-manage-1") 
-              strmlist$content # [1] "demo-1"   "demo-2"   "rest-in"  "rest-out"
+# Instance of R6 class 'ModelManageApi', with parent class/instance = 'ApiClient'/'api_cli'
+  # instantiating class requires "parent class" argument
+modman <- swagger::ModelManageApi$new(apiClient = api_cli) 
+  
+  modlist <- modman$model_list(instance = "model-manage-1") 
+    modlist$content # [1] "hello-world"
+    modman$model_get(instance = "model-manage-1", model = "hello-world") 
+    modman$stream_get(instance = "model-manage-1", stream = "rest-in")
+
+  strmlist <- modman$stream_list(instance = "model-manage-1") 
+    strmlist$content # [1] "demo-1"   "demo-2"   "rest-in"  "rest-out"
 
 # EngingeApi$new(...) =========
-  eng <- swagger::EngineApi$new(apiClient = swg)
+eng <- swagger::EngineApi$new(apiClient = api_cli)
+
+# Old naming (saved just in case!) ======
+# ApiClient$new(...) 
+# ConnectApi$new(...) 
+# ModelManageApi$new(...) 
+# EngingeApi$new(...) 
--- scratch.R
+++ scratch.R
@@ -1,6 +1,6 @@
 # Scratch
 
-# 1 ======
+# 1 string functions ======
 paste()
 paste0()
 file.path()
@@ -16,7 +16,8 @@ paste("~/a/very/long/path/here",
 ?cat
 
 
-# 2 =========
+# 1.5 if{} else{}
+# 2 return() =========
 .test <- function() "This work?"
 test()
 
@@ -37,18 +38,59 @@ t2 <- function(){
   for(i in names(dat)){
     d[[paste(i)]] <- i
   }
-  return(d)
+  d
 }
 t2()
+
+# "If there are no explicit returns from a function, the value of the last evaluated expression is returned automatically in R."
 
 # 3: FastScoreError$new() ======
 fse <- FastScoreError$new(message = "Unable to retrieve active sensors", caused_by = "This was caused by something.")
 fse$error_string()
 FastScoreError$new(message = "Unable to retrieve active sensors", caused_by = "This was caused by something.")$error_string()
 
-# 4: InstanceBase$new() --> active_sensors() =======
+# 4: InstanceBase$new()$active_sensors() =======
+
 x <- InstanceBase$new(name = "Betsy")
 InstanceBase$new(name = "Betsy")$active_sensors()
 x$active_sensors()
 
-# 5: InstanceBase$new() --> tapping_points =======
+# 5: InstanceBase$new()$tapping_points() =======
+x <- InstanceBase$new(name = "Besty")
+x$tapping_points()
+InstanceBase$new(
+  
+)$tapping_points()
+
+
+# 6: InstanceBase$new()$un/install_sensor() ====
+b <- InstanceBase$new(name = "Betsy")
+name
+b$name
+# 6.01: InstanceBase$new ========
+# api_cli <- InstanceBase$new(name = "FS") # don't need this; see 7
+
+# 6.02: parse_url(), build_url() ======
+A <- httr::parse_url("https://localhost/api/1/service")
+B <- httr::parse_url("https://localhost:15080")
+
+A$port <- B$port
+httr::build_url(A)
+# 7: Connect$new() =====
+#     Proxy prefix = https://localhost:15080
+#     Base path = api/1/service
+
+httr::set_config(httr::config(ssl_verifypeer = FALSE))
+
+api <- fastscore::InstanceBase$new()
+con <- fastscore::Connect$new(proxy_prefix = "https://localhost:15080")
+
+  A <- parse_url(con$apiClient$basePath) # [1] "https://localhost/api/1/service"
+  B <- parse_url(con$proxy_prefix) # [1] "https://localhost:15080"
+  A$port <- B$port
+  con$apiClient$basePath <- build_url(A)
+  
+  health <- con$health_get(instance = "connect")
+
+
+
