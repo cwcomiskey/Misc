efcddd4be1e4d52aa2353745f4f0455697e63aee
Chris Comiskey <cwcomiskey@gmail.com>
2018-01-25 14:46:49 -0800

    InstanceBase(...), Connect(...), ModelManage(...)

--- scratch.R
+++ scratch.R
@@ -1,6 +1,8 @@
 # Scratch
 
-# 1 string functions ======
+httr::set_config(httr::config(ssl_verifypeer = FALSE)) # global ignore-self-certify config
+
+# 1: string functions ======
 paste()
 paste0()
 file.path()
@@ -15,9 +17,15 @@ paste("~/a/very/long/path/here",
 
 ?cat
 
+# gsub(...)
+# gsub(pattern, replacement, x) 
+# look for 'pattern' in 'x' and replace it with 'replacement'
+urlPath <- "/{instance}/1/connect"
+instance <- "woohoo"
+gsub(paste0("\\{", "instance", "\\}"), `instance`, urlPath)
 
-# 1.5 if{} else{}
-# 2 return() =========
+# 1.5: if{} else{}
+# 2: return() =========
 .test <- function() "This work?"
 test()
 
@@ -45,9 +53,11 @@ t2()
 # "If there are no explicit returns from a function, the value of the last evaluated expression is returned automatically in R."
 
 # 3: FastScoreError$new() ======
-fse <- FastScoreError$new(message = "Unable to retrieve active sensors", caused_by = "This was caused by something.")
+fse <- FastScoreError$new(message = "Unable to retrieve active sensors", 
+                          caused_by = "This was caused by something.")
 fse$error_string()
-FastScoreError$new(message = "Unable to retrieve active sensors", caused_by = "This was caused by something.")$error_string()
+FastScoreError$new(message = "Unable to retrieve active sensors", 
+                   caused_by = "This was caused by something.")$error_string()
 
 # 4: InstanceBase$new()$active_sensors() =======
 
@@ -55,7 +65,7 @@ x <- InstanceBase$new(name = "Betsy")
 InstanceBase$new(name = "Betsy")$active_sensors()
 x$active_sensors()
 
-# 5: InstanceBase$new()$tapping_points() =======
+# 5:  $tapping_points() =======
 x <- InstanceBase$new(name = "Besty")
 x$tapping_points()
 InstanceBase$new(
@@ -63,16 +73,26 @@ InstanceBase$new(
 )$tapping_points()
 
 
-# 6: InstanceBase$new()$un/install_sensor() ====
+# 6:  $install_sensor() ====
 b <- InstanceBase$new(name = "Betsy")
 name
 b$name
-# 6.01: InstanceBase$new ========
+# 6.1: InstanceBase$new ========
 # api_cli <- InstanceBase$new(name = "FS") # don't need this; see 7
 
-# 6.02: parse_url(), build_url() ======
+# 6.2: parse_url(), build_url() ======
+httr::parse_url("https://localhost:15080/api/1/service")
+  # $scheme
+  # [1] "https"
+  # $hostname
+  # [1] "localhost"
+  # $port
+  # [1] "15080"
+  # $path
+  # [1] "api/1/service"
 A <- httr::parse_url("https://localhost/api/1/service")
 B <- httr::parse_url("https://localhost:15080")
+
 
 A$port <- B$port
 httr::build_url(A)
@@ -82,7 +102,6 @@ httr::build_url(A)
 
 httr::set_config(httr::config(ssl_verifypeer = FALSE))
 
-api <- fastscore::InstanceBase$new()
 con <- fastscore::Connect$new(proxy_prefix = "https://localhost:15080")
 
   A <- parse_url(con$apiClient$basePath) # [1] "https://localhost/api/1/service"
@@ -94,3 +113,77 @@ con <- fastscore::Connect$new(proxy_prefix = "https://localhost:15080")
 
 
 
+
+# 7.1 missing() ====
+  
+  # Test 1
+  test <- function(A, B){
+    if (missing(A)) {A <- B}
+    A + B
+  }
+  
+  test(3, 4)
+  test(B = 4)
+  
+  # Test 2
+  test <- function(A = NA, B = NA){
+    if (missing(A)) {A <- B}
+    A + B
+  }
+  
+  test(3, 4)
+  test(B = 4)
+  
+  # Test 3
+  test <- function(A = NULL, B = NA){
+    if (missing(A)) {A <- B}
+    A + B
+  }
+  
+  test(3, 4)
+  test(B = 4)
+  
+# 8: Client API + Connect API ======
+
+# Client API
+api_cli <- InstanceBase$new() # Option 1
+# api_cli <- InstanceBase$new(basePath = "https://localhost:15080/api/1/service") # Option 2
+  
+# Connect API
+con <- fastscore::Connect$new(apiClient = api_cli, proxy_prefix = "https://localhost:15080") # Use previously instantiated client API
+# con <- fastscore::Connect$new(proxy_prefix = "https://localhost:15080") # No previously instantiated client API
+  
+# 9: Connect$fleet ======
+InstanceBase$new(basePath = "http://crap") # TODO: NEED TO MAKE THIS THROW FS ERROR
+
+api <- fastscore::InstanceBase$new(basePath = "https://localhost:15080")
+con <- fastscore::Connect$new(apiClient = api)
+  con$basePath 
+  con_get <- con$fleet(instance = "connect")
+
+# 9.01 Connect$fleet  Garbage =======
+# swagger::ConnectAPI$connect_get() excerpt ====== #
+  urlPath <- "/{instance}/1/connect"
+  if (!missing(`instance`)) {
+    urlPath <- gsub(paste0("\\{", "instance", "\\}"), `instance`, urlPath)
+  }
+  resp <- self$apiClient$callApi(url = paste0(self$apiClient$basePath, urlPath), method = "GET", ...)
+
+# Testing ======== # 
+  instance <- "connect"
+  urlPath <- "/{instance}/1/connect"
+  urlPath <- gsub(paste0("\\{", "instance", "\\}"), `instance`, urlPath); urlPath
+  paste0(con$apiClient$basePath, urlPath) # [1] "https://localhost:15080/connect/1/connect"
+  
+# 10: stopifnot() ======
+stopifnot(FALSE)
+stop(
+     FastScoreError$new(
+       message = "basePath must use HTTPS scheme, e.g. https://dashboard:8000")$error_string()
+     )
+
+# 11: ModelManage$new() =====
+api <- fastscore::InstanceBase$new(basePath = "https://localhost:15080")
+modman <- fastscore::ModelManage$new(apiClient = api)  
+  modman$model_list(instance = "model-manage-1")
+  
