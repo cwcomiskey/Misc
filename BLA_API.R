cf4c9343c5eff22841cec7fd9630aa16207b711c
Chris Comiskey <cwcomiskey@gmail.com>
2018-04-12 15:51:17 -0700

    "major group" needle in haystack

--- BLA_API.R
+++ BLA_API.R
@@ -14,6 +14,20 @@ library(lubridate)
 
 # My BLS v2 API key: 968f207631b149269ac5603c9424c35c
 
+# CU = consumer price index
+
+# base_code
+# A = Alternate	
+# S = Current
+# 
+# periodicity_code	
+# R = Monthly	
+# S = Semi-Annual
+# 
+# seasonal_code     
+# S = Seasonally-adjusted
+# U  = Unadjusted
+
 # blsAPI package ======
 # Ex 1
 response <- blsAPI('LAUCN040010000000005', api_version = 2)
@@ -46,7 +60,7 @@ resp <- httr::POST(
 
 fromJSON(content(resp, "text"))
 
-# httr, BLS v2 API=========
+# httr -- BLS v2 API=========
 
 # one series: GET request
 # multiple series: POST request
@@ -81,11 +95,12 @@ dat2 <- dat2 %>% mutate(date = ymd(paste0(year, month, "01")),
 ggplot(data = dat2) + 
   geom_line(aes(x = date, y = value))
 
-# httr, .txt from BLS =====
+# httr -- .txt from BLS, example =====
 
 # https://download.bls.gov/pub/time.series/cu/
 # https://download.bls.gov/pub/time.series/ap/
 
+# e.g.
 dat <- httr::GET(
   url = "https://download.bls.gov/pub/time.series/cu/cu.data.11.USFoodBeverage") 
 dat <- content(dat, "text")
@@ -135,26 +150,77 @@ ggplot(data = dat2) +
   facet_wrap(~ series_id, scales = "free")
 
 
-# area_codes  =====
+# area_codes: cu.area  =====
 area_dat <- httr::GET(
   url = "https://download.bls.gov/pub/time.series/cu/cu.area") %>% content("text") %>% data.table::fread() %>% select(V1, V2, V3) 
 colnames(area_dat) <- c("area_code", "area_name", "display_level")
 
-# item_codes ====
+# item_codes: cu.item =========================================
 # https://download.bls.gov/pub/time.series/cu/cu.item
 item_codes <- httr::GET(
   url = "https://download.bls.gov/pub/time.series/cu/cu.item") %>% content("text") %>% data.table::fread() %>% select(V1, V2, V3) 
 colnames(item_codes) <- c("item_code", "item_name",	"display_level")
 
-filter(item_codes, display_level == 0)
+# table(item_codes$display_level)
+#  0   1   2   3   4   5   6   7   8 
+# 12  66  70 113  43  37  37  18   3 
 
-# httr, cu.series, Rosetta Stone ======
+filter(item_codes, display_level == "0")
+
+# meta-data: cu.series =================================
 
 # https://download.bls.gov/pub/time.series/cu/cu.series
 
-dat <- httr::GET(
-  url = "https://download.bls.gov/pub/time.series/cu/cu.series") %>% content("text") %>% data.table::fread()
+meta_dat <- httr::GET(
+  url = "https://download.bls.gov/pub/time.series/cu/cu.series") %>% 
+  content("text") %>% 
+  data.table::fread()
 
-names(dat)
-length(unique(dat$area_code))
-filter(dat, item_code == "SAA", area_code == "0000")
+names(meta_dat)
+length(unique(meta_dat$area_code)) # [1] 58 areas
+length(unique(meta_dat$item_code)) # [1] 399 items
+
+# all-items (one) series: cu.data.1.AllItems =================================
+all <- httr::GET(
+  url = "https://download.bls.gov/pub/time.series/cu/cu.data.1.AllItems") %>% 
+  content("text") %>% 
+  data.table::fread()
+
+length(unique(all$series_id)) # [1] 190
+table(all$series_id)
+
+
+
+
+# The rest =======================================
+# https://download.bls.gov/pub/time.series/cu/ + ...
+ends <- c("cu.data.11.USFoodBeverage", "cu.data.12.USHousing", 
+          "cu.data.13.USApparel", "cu.data.14.USTransportation", 
+          "cu.data.15.USMedical", "cu.data.16.USRecreation", 
+          "cu.data.17.USEducationAndCommunication", 
+          "cu.data.18.USOtherGoodsAndServices")
+d <- list()
+for(i in 1:length(ends)){
+  d[[i]] <- httr::GET(
+    url = paste0("https://download.bls.gov/pub/time.series/cu/", ends[i])) %>% 
+    content("text") %>% 
+    data.table::fread() 
+}
+
+FB <- d[[1]]
+sum(as.numeric(grepl("0000", FB$series_id))) # "0000" in Food&Bev series IDs
+length(unique(FB$series_id))
+
+# Primary (level 0) categories =================
+level0 <- filter(item_codes, display_level == 0) %>% .[c(3,5:12),]
+
+level0.2 <- filter(meta_dat, 
+                   item_code %in% level0$item_code, # "Major Group" item code
+                   area_code == "0000",             # Nat'l avg.
+                   seasonal == "U",
+                   periodicity_code == "R")
+
+# Food and Beverage index (calling it level 0 index) ("Major Groups")
+FB_0 <- FB %>% filter(series_id %in% level0.2$series_id) # YESSSSS!!!!!
+length(unique(FB_0$series_id))
+
