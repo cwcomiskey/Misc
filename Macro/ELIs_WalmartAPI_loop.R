c91e705541713e41386e53ea1253256aa2af5f14
Chris Comiskey <cwcomiskey@gmail.com>
2018-06-21 14:54:46 -0700

    looper duper changes

--- Macro/ELIs_WalmartAPI_loop.R
+++ Macro/ELIs_WalmartAPI_loop.R
@@ -16,6 +16,7 @@ bread <- walmartAPI::searching(
   query = "bread", key = key, categoryId = "976759", 
   numItems = 10, sort = "relevance")
 
+# Automate =====
 grab <- function(item, catId = NULL){
   t <- walmartAPI::searching(
     query = item, 
@@ -32,10 +33,7 @@ grab <- function(item, catId = NULL){
     .[1:5,]
   return(t)
 }
-
-t <- grab("shirt", "5438")
-
-# Automate? =====
+loader <- function(){
 Appendix5 <- read.csv("~/Desktop/ODG/Macro-models/Appendix5.txt", sep="") %>%
   mutate(Category = as.character(Category), Code = as.character(Code)) %>%
   filter(nchar(Code) == 5,
@@ -62,5 +60,31 @@ Appendix5 <- read.csv("~/Desktop/ODG/Macro-models/Appendix5.txt", sep="") %>%
     Category = str_replace(Category, " AT HOME", ""),
     Category = str_replace(Category, "UNCOOKED", "")
   )
-
+return(Appendix5)
+}; Appendix5 <- loader(); rm(loader)
+ELI_vector <- function(){
 items <- c("flour", "cereal", "rice", "pasta", "cornmeal", "bread", "biscuits", "muffins", "cake", "cupcakes", "cookies", "crackers", "coffee cake", "doughnuts", "pie", "tart", "ground beef", "beef roast", "beef steak", "ham", "pork chops", "frankfurters", "lunchmeat", "lamb", "chicken", "fish", "seafood", "eggs", "milk", "apples", "bananas", "citrus fruit", "potatoes", "lettuce", "tomatoes", "frozen fruit", "frozen vegetables", "coffee", "tea", "sugar", "candy", "butter", "margarine", "mayonnaise", "salad dressing", "soup", "dried fruit", "snacks", "olives", "pickles", "sauce", "baby food", "salad", "spirits", "wine", "alcoholic beverage", "oil", "curtains", "drapes", "linens", "bathroom linens", "bedroom linens", "kitchen linens", "dining room linens", "mattress", "sofa", "furniture cover", "decorative pillows", "chairs", "tables", "kitchen furniture", "dining room furniture", "infants furniture", "outdoor furniture", "refrigerators", "home freezer", "laundry machine", "drier", "range", "cooktop", "microwave", "kitchen appliances", "dishwashwer", "lamps", "lighting fixture", "clock", "plant", "flowers", "dishes", "flatware", "tableware", "kitchenware", "paint", "wallpaper", "power tools", "handtools", "cleaning supplies", "cleaning products", "household paper", "moving boxes", "men's suits", "sport coat", "men's outerwear", "men's underwear", "men's activewear", "men's shirt", "men's sweater", "men's vest", "men's pants", "men's shorts", "boy outerware", "boys shirts", "boys sweaters", "boys underwear", "boys suits", "boys pants", "boys sportwear", "women outerwear", "women dresses", "womens tops", "skirts", "women's pants", "womens shorts", "women's suits", "women's hosiery", "women's sportswear", "girls outerwear", "girls dresses", "girls tops", "girls skirts", "girls pants", "girls shorts", "girls sportswear", "girls underwear", "men's footwear", "boys footwear", "girls footwear", "women's footwear", "infant clothing", "infant outerwear", "toddler clothing", "toddler outerwear", "watches", "jewelry", "tires", "motor oil", "coolant", "prescription drugs", "drugs", "nonprescription drugs", "first-aid", "televisions", "DVDs", "Pet food", "pet supplies", "bicycles", "hunting", "fishing", "camping", "film", "photography", "toys", "games", "playground", "video games", "software", "sewing", "music", "music instruments", "magazines", "books", "school supplies", "computer", "phone", "smartphone", "cigarettes", "shaving", "cosmetics", "perfume", "nails", "stationary", "gift wrap", "wrapping paper", "luggage")
+return(items)}; items <- ELI_vector(); rm(ELI_vector)
+
+for(i in 1:length(items)){
+  if(i == 1) dat <- data.frame()
+
+  dat_i <- tryCatch(
+    grab(items[i]),
+    error = function(c){
+      data.frame(itemId = i, 
+                 name = items[i], 
+                 salePrice = paste(c), 
+                 categoryPath = NA, 
+                 productUrl = NA, 
+                 offerType = NA)
+      }
+    )
+  
+  if(i %% 5 == 0) print(i)
+  
+  dat <- rbind.data.frame(dat, dat_i)
+  
+  if(i == length(items)) rm(i, dat_i)
+}
+
