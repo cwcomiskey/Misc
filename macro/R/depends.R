3e73d2f47bf952aa19c39864a87ef91c7c0b79fe
Chris Comiskey <cwcomiskey@gmail.com>
2018-06-28 13:14:44 -0700

    Macro project, help build cpir, shenanigans

---
+++ macro/R/depends.R
@@ -0,0 +1,11 @@
+#' @export
+depends <- function() {
+  library(data.table)
+  library(readr); library(stringr)
+  library(dplyr); library(lubridate)
+  library(httr); library(jsonlite)
+  library(ggplot2); library(ggfortify)
+  library(tidyverse)
+  library(forecast)
+
+}
