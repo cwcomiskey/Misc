01f8bcebb02906c429a52ea20d185cc818cbe719
Chris Comiskey <cwcomiskey@gmail.com>
2018-06-28 13:11:45 -0700

    Walmart API scraping package

---
+++ scrapr/R/data.R
@@ -0,0 +1,16 @@
+#' My Walmart API Key
+"key"
+
+#' 184 Entry Level Items, by name
+"ELI"
+
+#' 884 Walmart Item IDs
+"dat_itemIDs"
+
+#' 920 x 8: Walmart API search by Name
+"dat"
+
+#' 884 x 8: Walmart API search by Item ID
+"datr"
+
+
