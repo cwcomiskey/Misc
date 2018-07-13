f6df6c00e97546f5898fb1d255223bae21db0cfe
Chris Comiskey <cwcomiskey@gmail.com>
2018-07-13 15:45:16 -0700

    Successful scrape of ~5 motorcycle prices

--- Macro/motorcycles.R
+++ Macro/motorcycles.R
@@ -26,17 +26,46 @@ title
 
 stats <- xml2::read_html("https://www.motorcyclistonline.com/2018-honda-xr650l") %>%
   rvest::html_nodes("div.buyers-guide--intro-stats-item") %>%
-  html_text() 
-stats
-cat(stats)
-
-stats <- xml2::read_html("https://www.motorcyclistonline.com/2018-honda-xr650l") %>%
-  rvest::html_nodes("div.buyers-guide--intro-stats-item") %>%
   html_text() %>%  
   strsplit(split = "\n") %>%
   unlist() %>% 
   stringr::str_trim() %>%
-  .[. != ""]
+  .[. != ""] %>%
+  grep("\\$", ., value = TRUE)
 
 
+# function-ify =====
+type <- "2018-honda-xr650l"
 
+# Year-manufacturer-model (or whatever)
+types <- c("2018-honda-xr650l", "2014-husaberg-fe-501", "2018-aprilia-rsv4-rf", "2018-beta-evo-250-factory", "2018-bmw-r-1200-gs-adventure")
+
+zoom <- function(type){
+  
+  url <- paste0("https://www.motorcyclistonline.com/", type)
+  
+  cycle <- xml2::read_html(url) %>%
+    rvest::html_nodes("h1.page-title") %>%
+    html_text() 
+  
+  price <- xml2::read_html(url) %>%
+    rvest::html_nodes("div.buyers-guide--intro-stats-item") %>%
+    html_text() %>%  
+    strsplit(split = "\n") %>%
+    unlist() %>% 
+    stringr::str_trim() %>%
+    .[. != ""] %>%
+    grep("\\$", ., value = TRUE)
+  
+  return(data.frame(cycle, price))
+  
+}
+
+bikes <- data.frame()
+for(i in types) {
+  bikes_i <- zoom(i)
+  bikes <- rbind.data.frame(bikes, bikes_i)
+}
+bikes
+
+# HUZZAH!!
