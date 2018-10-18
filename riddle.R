6c0e4874089eed97faf691ed6439ca4478c92a71
Chris Comiskey <cwcomiskey@gmail.com>
2018-10-18 16:00:21 -0700

    Add "if else" for d = 1, >1

--- riddle.R
+++ riddle.R
@@ -21,7 +21,6 @@ PW <- function(w, x, y, z){
   return(pwin)
   } # P(W = w wins|x, y, z)
 
-
 Optd <- function(a, b, c){
   X <- data.frame(Interval = c("1", "2", "3", "4"), Score = 0) 
   x <- sort(c(a, b, c))
@@ -31,7 +30,6 @@ Optd <- function(a, b, c){
   X["4", "Score"] <- 1 - x[3]
   winner <- filter(X, Score == max(Score))$Interval
   
-  if(length(winner) == 1) stop("No winner, this is a problem.")
   if(length(winner) == 1){
     if(winner == "4"){
       d <- x[3] + .Machine$double.eps
@@ -42,21 +40,32 @@ Optd <- function(a, b, c){
     } else{
         d <- x[1] - .Machine$double.eps
     }
-  }
+    return(d)
+    }
   if(length(winner) > 1){
     l <- length(winner)
-    #
-    #  --- CONTINUE HERE ---
-    # 
+    d <- 0
+    for(index in 1:l){
+        if(winner[index] == "4"){
+          d[index] <- x[3] + .Machine$double.eps
+        } else if(winner[index] == "3"){
+          d[index] <- 0.5*(x[2] + x[3])
+        } else if(winner[index] == "2"){
+          d[index] <- 0.5*(x[1] + x[2])
+        } else{
+          d[index] <- x[1] - .Machine$double.eps
+        }
+    }
+    return(d)
   }
-  return(d)
-} # optimal d given a, b, c
+    } # optimal d(s) given a, b, c
 
 l <- 100 # length
-I05 <- seq(0, 0.5, length = l) # [0, 0.5]
-I01 <- seq(0, 1, length = l)   # [0, 1]
+I05 <- seq(0, 0.5, length = l) # [0, ... , 0.5]
+I01 <- seq(0, 1, length = l)   # [0, ... , 1]
 
 for(ai in I05){
+  print(ai)
   
   for(bj in I01){
     if(bj == ai) next
@@ -65,15 +74,28 @@ for(ai in I05){
       if(ck == ai | ck == bj) next
       
       d_opt <- Optd(a = ai, b = bj, c = ck)
-      
-      PA <- PW(ai, bj, ck, d_opt)
-      PB <- PW(bj, ai, ck, d_opt)
-      PC <- PW(ck, ai, bj, d_opt)
-      PD <- PW(d_opt, ai, bj, ck)
-      
-      result_ijk <- data.frame(a = ai, b = bj, c = ck, d = d_opt,
-                               PA = PA, PB = PB, PC = PC, PD = PD)
-      container <- rbind.data.frame(container, result_ijk)
+      if(length(d_opt) == 1){
+        PA <- PW(w = ai, bj, ck, d_opt)
+        PB <- PW(w = bj, ai, ck, d_opt)
+        PC <- PW(w = ck, ai, bj, d_opt)
+        PD <- PW(w = d_opt, ai, bj, ck)
+        
+        result_ijk <- data.frame(a = ai, b = bj, c = ck, d = d_opt,
+                                 PA = PA, PB = PB, PC = PC, PD = PD)
+        container <- rbind.data.frame(container, result_ijk)
+      } else if(length(d_opt) > 1){
+        len <- length(d_opt)
+        for(index in 1:len){
+          PA <- PW(w = ai, bj, ck, d_opt[index])
+          PB <- PW(w = bj, ai, ck, d_opt[index])
+          PC <- PW(w = ck, ai, bj, d_opt[index])
+          PD <- PW(w = d_opt[index], ai, bj, ck)
+          
+          tie_ijk <- data.frame(a = ai, b = bj, c = ck, d = d_opt[index],
+                                   PA = PA, PB = PB, PC = PC, PD = PD)
+          container <- rbind.data.frame(container, tie_ijk)
+        }
+      }
 
     }
   }
